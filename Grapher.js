17cf9d3c399c0142418dda9c5bb43c6a51120df4
Ofek Gila <ofek@theofekfoundation.org>
2016-11-10 20:03:23 -0800

    First jquery -> js commit

--- Grapher.js
+++ Grapher.js
@@ -299,8 +299,8 @@ function pageReady() {
 	$("#upper-footer").css('bottom', $("#footer").outerHeight());
 	$("#upper-upper-footer").css('bottom', $("#footer").outerHeight() + $("#upper-footer").outerHeight());
 
-	docwidth = $("#content-wrapper").outerWidth(true);
-	docheight = $("#content-wrapper").outerHeight(true);
+	docwidth = getElemWidth(document.getElementById('content-wrapper'));
+	docheight = getElemHeight(document.getElementById('content-wrapper'));
 
 	graph.setAttribute('width', docwidth);
 	graph.setAttribute('height', docheight);
